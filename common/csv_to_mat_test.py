be2a6414e79d08a47951486fc1d6a11e70dd6e47
yshao <yshaoeng@gmail.com>
2015-01-12 13:47:31 -0700

    Update

--- common/csv_to_mat_test.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-import scipy
-
-
-filep='file'
-basename=os.path.basename(filep)
-data = pd.io.read_csv(basename+".csv",parse_dates=True)
-scipy.io.savemat(basename+'.mat',data)
