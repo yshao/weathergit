6bfd87e1aedd9c3fa867d9fe43d50505272b90c1
yshao <yshaoeng@gmail.com>
2015-01-19 11:53:51 -0700

    Update:

--- common/tests/test_smaputils.py
+++ common/tests/test_smaputils.py
@@ -1,5 +1,6 @@
 import re
 import unittest
+from weathergit.common.dataclient import DataClient
 from weathergit.common.config import Config
 from weathergit.common.smaputils import SmapUtils
 
@@ -14,13 +15,55 @@ POINT=dict(
 )
 
 
-class SmapUtilsTest(unittest.TestCase):
-    def setUp(self):
-        self.config=Config("../../common/weatherplotter.conf")
-        self.c=SmapUtils()
+def export_csv():
+    dc=DataClient()
 
-    def test_get_curr_val(self):
-        ""
-        self.c.get_curr_val('/garmin0/latitude')
-        self.c.get_curr_val('/garmin0/longitude')
+    opt='EXCEL'
+    data=dc.load_data()
+    if opt == 'EXCEL':
+        from xlwings import Workbook, Sheet, Range
+        import numpy as np
+        wb=Workbook("test.xlsx")
+        wb.caller()
+        n = Range('Sheet1', 'B1').value  # Write desired dimensions into Cell B1
+        rand_num = np.random.randn(n, n)
+        Range('Sheet1', 'C3').value = rand_num
 
+def load_csv(filep,streamp):
+    ""
+
+
+export_csv()
+
+# class SmapUtilsTest(unittest.TestCase):
+#     EXCEL=1
+#
+#     def setUp(self):
+#         self.config=Config("../../common/weatherplotter.conf")
+#         self.c=SmapUtils()
+#
+#     def test_get_curr_val(self):
+#         ""
+#         self.c.get_curr_val('/garmin0/latitude')
+#         self.c.get_curr_val('/garmin0/longitude')
+#
+#     def test_load_csv(self):
+#         ""
+#
+#     def test_export_csv(self):
+#         ""
+#
+#         # self.c.
+#         # opts={'EXCEL',excel}
+#         # value=
+#         # method_name = 'visit_' + str(value)
+#         # switch (opts)
+#         #
+#         # case EXCEL:
+#         #
+#         # case CSV:
+#
+#
+#
+#
+#
