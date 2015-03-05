de76873b942ce00e061f1d340614fa8fc1d910e4
yshao <yshaoeng@gmail.com>
2015-03-04 17:50:44 -0700

    Update

--- 030415/processdriver.py
+++ 030415/processdriver.py
@@ -1,4 +1,37 @@
+from smap.archiver.client import SmapClient, RepublishClient
+from smap.operators import OperatorDriver
+
 __company__ = 'Boulder Environmental Sciences and Technology'
 __project__ = ''
 __author__ = 'Y. Shao'
 __created__ = '3/4/2015' '11:16 AM'
+
+
+HOST='http://192.168.1.120:8079'
+source_ids=['']
+
+class ProcessDriver(OperatorDriver):
+    def setup(self,opts):
+        ""
+
+        restrict=opts.get('Restrict','')
+        # client=SmapClient()
+        client=SmapClient(HOST)
+        source_ids=client.tags(restrict,'uuid, Properties/UnitofMeasure')
+        for id in source_ids:
+            ""
+            uuid=str(id['uuid'])
+            if not 'Properties/UnitofMeasure' in id:
+                id['Properties/UnitofMeasure'] = ''
+            if not uuid in self.operators:
+                ""
+
+        RepublishClient()
+
+
+    def process(self):
+        self.op='thetaprobe'
+
+
+    def update(self):
+        ""
--- 224/installer_build.py
+++
@@ -1,4 +0,0 @@
-__company__ = 'Boulder Environmental Sciences and Technology'
-__project__ = ''
-__author__ = 'Y. Shao'
-__created__ = '2/24/2015' '3:02 PM'
