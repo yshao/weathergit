93e9ad722c122177915879d3346a502e58d2271e
yupings <yshaoeng@gmail.com>
2015-03-11 10:31:18 -0600

    Update

--- smapserver/event/eventhandlers.py
+++ smapserver/event/eventhandlers.py
@@ -1,24 +1,81 @@
 import datetime
+from common.env import Env
+from common.remote.remote import Remote
+from common.smaputils import SmapUtils
 
 __company__ = 'Boulder Environmental Sciences and Technology'
 __project__ = ''
 __author__ = 'Y. Shao'
 __created__ = '3/2/2015' '5:14 PM'
 
+import pickle
+
+server_service=list(
+    'trendnet.ini'
+    'webserver.py'
+)
+
+smap_service=list(
+    'weather.ini'
+)
+
+def watchdog():
+    if unable_to_connect():
+        ""
+        send_event_email('Network Down %s')
+    ### find all the service is running
+    remote=Remote(Remote.gen_login('smapsource'))
+    pid=remote.get_pids()
+    for s in server_service:
+        if s not in pid.keys():
+            remote.daemon()
+
+    remote=Remote(Remote.gen_login('smapserver'))
+    for s in server_service:
+        if !check_pid(s)
+            start()
+
+    if frozen_measurements():
+        ""
+
+
+    if disk_level():
+        flush_smap_disk()
+        send_event_email('Disk buffer flushed')
+
+    update_status()
+
+def compareUpdate(l1,l2):
+    def convert_tm(tm):
+        return datetime.timedelta
+
+    for k in l1.keys():
+        if convert_tm(l1.get(k)) - convert_tm(l2.get(k)) > 0:
+            return False
+
+    return True
+
 def frozen_measurements():
   """
   Check if encounters measurements not updating
   Restart
   """
-  def convert_time():
-      ""
 
+  d=Env().getConfig()
+  args=dict(url=d['smap_server_host'])
+  smaputils=SmapUtils(args)
+  mon=smaputils.get_smap_monitor()
 
-  curr=convert_time([])
-  if curr_measurements['time'] - datetime.dateime.now() > 5
+  prev=pickle.load('z1')
+  timeval=mon
+  pickle.dump('z1',timeval)
+
+  #action
+  if compareUpdate(prev,timeval):
     return True
   else:
     return False
+
 
 
 # def handle_instruments():
@@ -45,29 +102,17 @@ def unable_to_connect():
     return False
 
 
-def unable_to_read_status():
-  """
-  Check for network connectivity.
-  Notify
-  """
-
-  if ():
-    return True
-  else
-    return False
-
-
 def disk_level():
   """
 
   flush,notify
   """
 
-  d=show_disk()
+  d=diskmain()
   if d['space']:
     return True
   else:
-    return False
+    flush_smap_disk()
 
 ############ disk_level #########
 def flush_smap_disk():
--- smapserver/event/snapshot_disp_test.py
+++ smapserver/event/snapshot_disp_test.py
@@ -1,8 +1,12 @@
+import os
+from common.env import Env
 from common.smaputils import SmapUtils
 from common.remote.remote import Remote
 
 
-def webgen():
+def update_webpage():
+    ""
+
     d=Env().getConfig()
 
     args=dict(url=d['smap_source_host'])
@@ -13,22 +17,35 @@ def webgen():
     gen['lat']=smaputils.get_curr_val('/garmin0/latitude')
     gen['alt']=smaputils.get_curr_val('/garmin0/altitude')
 
-
-    with open('panel','wb') as f:
+    GENDIR='generate'
+    with open('%s/panel.txt'%GENDIR,'wb') as f:
         f.write(gen)
 
     # get image from database
     filep=smaputils.get_curr_val('/trendnet0/timestamp')
-
-    #
-    remote=Remote.gen_login('data_server')
-    d=remote.gen_login('data_server')
-    remote.
+    d=Remote.gen_login('dataserver')
+    dataserver=Remote(d)
+    dataserver.download('%s.png'%filep,GENDIR)
 
     # get plots
-    paths=['']
+    paths=[
+        '/thetaprobe0/soil_moistureHI',
+        '/omega0/soil_tempHI',
+        '/vaisala0/pth/pressure',
+        '/vaisala0/pth/temperature',
+        '/vaisala0/pth/humidity'
+    ]
     for p in paths:
         ""
+        gen_plot(p)
 
-    def get_smap_values():
-        ""
+
+    # upload all to webserver
+    d=Remote.gen_login('webserver')
+    remote=Remote(d)
+
+    lFiles=os.list(GENDIR)
+    remote.upload(lFiles)
+
+def gen_plot(p):
+    name=p.replace('/','_')
